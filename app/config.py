5c3738f7f2684727bd28c7567b7bb2571c532c21
Eugene <rheingoldgalaxy@gmail.com>
2022-07-18 17:45:37 +0300

    simple form and flash messages

--- app/config.py
+++ app/config.py
@@ -4,4 +4,4 @@
 class Configuration(object):
     DEBUG = True
 
-    
+    SECRET_KEY = 'pgkluchgfuromgnkrpfwkhofuhipfg'
--- app/flsite.py
+++ app/flsite.py
@@ -1,12 +1,15 @@
 from config import Configuration
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, flash
 
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
 
 
-menu = ['install', 'first app', 'callback']
+
+menu = [{'name': 'install', 'url':'install-flask'},
+        {'name': 'firat app', 'url': 'fitst-app'},
+        {'name': 'contactus', 'url': 'contact'}]
 
 
 @app.route('/')
@@ -19,6 +22,20 @@ def index():
 def about():
     print(url_for('/about'))
     return render_template('about.html', title='about site', menu=menu)
+
+
+@app.route('/contact', methods=['POST', 'GET'])
+def contact():
+
+    
+
+    if request.method == 'POST':
+        if len(request.form['username']) > 2:
+            flash('message sent')
+        else:
+            flash('send error')
+        print(request.form)
+    return render_template('contactus.html', title='callback', menu=menu)
 
 
 
