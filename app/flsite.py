12bf0d346ca2778d1508674e1de6a423c6579716
Eugene <rheingoldgalaxy@gmail.com>
2022-07-19 10:02:41 +0300

    authorisation

--- app/flsite.py
+++ app/flsite.py
@@ -1,5 +1,9 @@
+from crypt import methods
+from unicodedata import category
+
+# from requests import session
 from config import Configuration
-from flask import Flask, render_template, url_for, request, flash
+from flask import Flask, render_template, url_for, request, session, flash, redirect, abort
 
 
 app = Flask(__name__)
@@ -26,16 +30,37 @@ def about():
 
 @app.route('/contact', methods=['POST', 'GET'])
 def contact():
-
-    
-
     if request.method == 'POST':
         if len(request.form['username']) > 2:
-            flash('message sent')
+            flash('message sent', category='success')
         else:
-            flash('send error')
+            flash('send error', category='error')
         print(request.form)
     return render_template('contactus.html', title='callback', menu=menu)
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if 'userLogged' in session:
+        return redirect(url_for('profile', username=session['userLogged']))
+    elif request.method == 'POST' and request.form['username'] == 'eugene' and request.form['psw'] == '123':
+        session['userLogged'] = request.form['username']
+        return redirect(url_for('profile', username=session['userLogged']))
+
+    return render_template('login.html', title='Authorisation', menu=menu)
+
+
+@app.route('/profile/<username>')
+def profile(username):
+    if 'userLogged' not in session or session['userLogged'] != username:
+        abort(401)
+    return f"{username}'s profile"
+
+
+
+@app.errorhandler(404)
+def pageNotFound(error):
+    return render_template('page404.html', title='Page not found', menu=menu), 404
 
 
 
