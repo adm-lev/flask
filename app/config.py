119cd877328b259a6df18cfd30ebfb38bab59422
Eugene <rheingoldgalaxy@gmail.com>
2022-07-21 17:34:11 +0300

    del old

--- app/config.py
+++
@@ -1,9 +0,0 @@
-
-
-
-class Configuration(object):
-    DEBUG = True
-
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://webadmin:Gfctrf-12@localhost/test1'
